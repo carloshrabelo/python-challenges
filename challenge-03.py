df36639c550cae1376ccfd916f0b407c71f656c7
Carlos Henrique <ext.carlos.holiveira@farfetch.com>
2022-06-17 22:03:02 +0100

    challenge 03

---
+++ challenge-03.py
@@ -0,0 +1,9 @@
+pi = 3.14159
+
+raio = input("Please enter value of n:\n")
+raio = float(raio)
+raio = "{:.4f}".format(raio)
+raio = float(raio)
+result = raio * raio * pi
+
+print("{:.4f}".format(result))
